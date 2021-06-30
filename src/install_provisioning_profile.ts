46cfa2eafc48fa5467f73445a2f1dd56345ec89e
matinzd <zadehdolatabad@gmail.com>
2021-06-30 11:54:43 +0200

    debug

--- src/install_provisioning_profile.ts
+++ src/install_provisioning_profile.ts
@@ -1,78 +1,94 @@
-import os = require('os');
-import core = require('@actions/core');
-import io = require('@actions/io');
-const exec = require('@actions/exec');
-import sign = require('./ios-common');
-import ioutils = require('./actions-common');
-import fs = require('fs');
-import path = require('path');
+import os = require("os");
+import core = require("@actions/core");
+import io = require("@actions/io");
+const exec = require("@actions/exec");
+import sign = require("./ios-common");
+import ioutils = require("./actions-common");
+import fs = require("fs");
 
 async function run() {
-   await installProvisioningProfileTask();
+  await installProvisioningProfileTask();
 }
 
 export async function installProvisioningProfileTask() {
-    try {        
-        // Check platform is macOS since demands are not evaluated on Hosted pools
-        if (os.platform() !== 'darwin') {
-            core.error('Install requires Mac runtime')
-            throw new Error('InstallRequiresMac');
-        }
-
-        // BASE64 encoded .mobileprovision passed through GitHub secret
-        let encodedProfileData = core.getInput('profile-base64');
-
-        // Temporary file locations
-        let provisioningProfileFile = '/tmp/profile.mobileprovision';
-        let base64ProfileFile = '/tmp/profile.base64';
-        
-        if (encodedProfileData) {
-           fs.writeFile(base64ProfileFile, encodedProfileData, async (err) => {
-                if (err) {
-                    core.error('could not write base64 provisioning file to /tmp');
-                }
-                else {
-                    let base64Cmd = await io.which('base64', true);
-                    await exec.exec(base64Cmd, ['-d', '-i', base64ProfileFile, '-o', provisioningProfileFile]);
-
-                    // remove base64 file
-                    io.rmRF(base64ProfileFile);
-                    core.debug('Removed base64 version of provisioning profile.');
-
-                    if (ioutils.exists(provisioningProfileFile) && ioutils.stats(provisioningProfileFile).isFile()) {
-                        core.debug('Found provisioning profile to install');
-                        try
-                        {
-                            const info = await sign.installProvisioningProfile(provisioningProfileFile);
-                            
-                            // set the provisioning profile output variable.
-                            core.debug('setting environment variables privisioningProfileUuid & Name');
-                            core.setOutput('APPLE_PROV_PROFILE_UUID', info.provProfileUUID);
-                            core.exportVariable('provisioningProfileUuid', info.provProfileUUID);
-                            core.exportVariable('provisioningProfileName', info.provProfileName || '');
-
-                        } catch (err) {
-                            core.error(err);
-                        } finally {
-                            // remove temporary file
-                            io.rmRF(provisioningProfileFile);
-                            core.debug('removed provisioning profile from /tmp');
-                        }
-                    }
-                    else {
-                        core.error('Input Provisioning Profile Not Found at ' + provisioningProfileFile);
-                        throw new Error('InputProvisioningProfileNotFound');
-                    }
-                }
-            });
-           
-        } else {
-            core.error('Required secret PROVISIONING_PROFILE is not defined.');
-            throw new Error('ProvisioningProfileSecretNotDefined');
-        }
-    } catch (err) {
-        core.setFailed("Task Failed");
+  try {
+    // Check platform is macOS since demands are not evaluated on Hosted pools
+    if (os.platform() !== "darwin") {
+      core.error("Install requires Mac runtime");
+      throw new Error("InstallRequiresMac");
     }
+
+    // BASE64 encoded .mobileprovision passed through GitHub secret
+    let encodedProfileData = core.getInput("profile-base64");
+
+    // Temporary file locations
+    let provisioningProfileFile = "/tmp/profile.mobileprovision";
+    let base64ProfileFile = "/tmp/profile.base64";
+
+    if (encodedProfileData) {
+      fs.writeFile(base64ProfileFile, encodedProfileData, async (err) => {
+        if (err) {
+          core.error("could not write base64 provisioning file to /tmp");
+        } else {
+          let base64Cmd = await io.which("base64", true);
+          await exec.exec(base64Cmd, [
+            "-d",
+            "-i",
+            base64ProfileFile,
+            "-o",
+            provisioningProfileFile,
+          ]);
+
+          // remove base64 file
+          io.rmRF(base64ProfileFile);
+          core.debug("Removed base64 version of provisioning profile.");
+
+          if (
+            ioutils.exists(provisioningProfileFile) &&
+            ioutils.stats(provisioningProfileFile).isFile()
+          ) {
+            core.debug("Found provisioning profile to install");
+            try {
+              const info = await sign.installProvisioningProfile(
+                provisioningProfileFile
+              );
+
+              // set the provisioning profile output variable.
+              core.debug(
+                "setting environment variables privisioningProfileUuid & Name"
+              );
+              core.setOutput("APPLE_PROV_PROFILE_UUID", info.provProfileUUID);
+              core.exportVariable(
+                "provisioningProfileUuid",
+                info.provProfileUUID
+              );
+              core.exportVariable(
+                "provisioningProfileName",
+                info.provProfileName || ""
+              );
+            } catch (err) {
+              core.error(err);
+            } finally {
+              // remove temporary file
+              io.rmRF(provisioningProfileFile);
+              core.debug("removed provisioning profile from /tmp");
+            }
+          } else {
+            core.error(
+              "Input Provisioning Profile Not Found at " +
+                provisioningProfileFile
+            );
+            throw new Error("InputProvisioningProfileNotFound");
+          }
+        }
+      });
+    } else {
+      core.error("Required secret PROVISIONING_PROFILE is not defined.");
+      throw new Error("ProvisioningProfileSecretNotDefined");
+    }
+  } catch (err) {
+    core.setFailed("Task Failed");
+  }
 }
 
 run();
